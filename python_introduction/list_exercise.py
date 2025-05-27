10020c2bbc9113e80292f1bbf99bd197f6a66c5b
Temitayo Shittu <temitayo.shittu7@gmail.com>
2025-05-27 20:28:32 +0100

    Add list_exercise.py to python_introduction

---
+++ python_introduction/list_exercise.py
@@ -0,0 +1,13 @@
+# even_count = 0
+# for number in range(1, 10):
+#   if number % 2 == 0:
+#     print(number)
+#     even_count += 1
+# print(f"Total even numbers: {even_count}")
+
+
+numbers = [1, 5, 3, 9]
+total = 0
+for number in numbers:
+  total += number
+print(total)
