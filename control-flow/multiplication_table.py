df715478bd8e87710928f88b236674e67085fa6e
Temitayo Shittu <temitayo.shittu7@gmail.com>
2025-05-27 22:03:44 +0100

    Modified the motiplication_table.py in control-flow

--- control-flow/multiplication_table.py
+++ control-flow/multiplication_table.py
@@ -1,4 +1,4 @@
 number = int(input("Enter a number to see its multiplication table: "))
-for num in range(1,11):
-  product = num * number
-  print(number,"*", num,"=",product)
+for i in range(1,11):
+  product = i * number
+  print(f"{number} * {i}")
