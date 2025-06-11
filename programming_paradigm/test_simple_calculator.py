ed5f1907ae38bc4a3351226a39817a0529673e73
Temitayo Shittu <temitayo.shittu7@gmail.com>
2025-06-11 12:49:30 +0100

    Modified test_simple_calculator.py

--- programming_paradigm/test_simple_calculator.py
+++ programming_paradigm/test_simple_calculator.py
@@ -11,10 +11,10 @@ class TestSimpleCalculator(unittest.TestCase):
   def test_subtraction(self):
     self.assertEqual(self.calc.subtract(8, 2), 6)
 
-  def test_multiply(self):
+  def test_multiplication(self):
     self.assertEqual(self.calc.multiply(8, 2), 16)
   
-  def test_divide(self):
+  def test_division(self):
     self.assertEqual(self.calc.divide(8, 2), 4)
 
 if __name__ == "__main__":
