93ea67bc5759c790159806aa3c1f2674b561116a
Temitayo Shittu <temitayo.shittu7@gmail.com>
2025-06-11 10:34:03 +0100

    Add robust_division_calculator.py and main.py to programming_paradigm folder

--- programming_paradigm/robust_division_calculator.py
+++ programming_paradigm/robust_division_calculator.py
@@ -3,6 +3,5 @@ def safe_divide(numerator, denominator):
     result = float(numerator) / float(denominator)
     return f"The result of the division is {result}"
   except ZeroDivisionError:
-    return "Error: Cannot divide by zero"
-  
+    return "Error: Cannot divide by zero."
   
